3892cdf1b04ad078da1fcd1f0f4d39a033b1bc7d
SIPDOMAIN\chuemimihtwee <chuemimihtwe5678@gmail.com>
2023-09-11 09:47:52 +0630

    feature : implement successfully connecting with documentum server by using DFC in maven project

---
+++ src/test/java/com/example/sample_dfc/SampleDfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample_dfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDfcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
