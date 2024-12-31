1d85749fcb884abc73cedbdb1329508b1185d3e1
SIPDOMAIN\chuemimihtwee <chuemimihtwe5678@gmail.com>
2024-12-31 14:27:30 +0630

    enhancement : switch back and forth between logback and log4j for dfc logging system

--- src/test/java/com/example/sample_dfc/SampleDfcApplicationTests.java
+++ src/test/java/com/example/sample_dfc/SampleDfcApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.sample_dfc;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SampleDfcApplicationTests {
+    private static final Logger logger = LoggerFactory.getLogger(SampleDfcApplicationTests.class);
 
     @Test
     void contextLoads() {
+        logger.info("Test log message");
     }
 
 }
